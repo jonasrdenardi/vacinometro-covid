6d1fd1c19c1c6be13c1ef58d4a50412f220f15f0
Jonas R. Denardi <36958021+jonasrdenardi@users.noreply.github.com>
2021-01-26 22:31:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <title>Vacinômetro Covid</title>
-    <script data-ad-client="ca-pub-6059926323992215" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <meta charset="utf-8">
-  </head>
-  <body>
-      <h1>Teste Vacinômetro</h1>    
-  </body>
+
+<head>
+  <title>Vacinômetro Covid</title>
+  <script data-ad-client="ca-pub-6059926323992215" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <?php
+  $xml = file_get_contents("https://www.saopaulo.sp.gov.br/");
+  echo "$xml";
+  ?>
+
+  <h1>Teste Vacinômetro</h1>
+</body>
+
 </html>
