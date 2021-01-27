4160edd98bf2d15ab3991439f67da0ba0b763b8e
Jonas R. Denardi <36958021+jonasrdenardi@users.noreply.github.com>
2021-01-26 22:14:57 -0300

    Inicio

---
+++ VacinometroCovid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VacinometroCovid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
